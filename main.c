e7af4bc3d033df1d4ee49a05926e6f188f2a0935
Diogo Leite <diogoleite87@gmail.com>
2022-10-12 03:16:28 -0300

    feat: menu add Hash Table

--- main.c
+++ main.c
@@ -107,7 +107,7 @@ int main() {
                 fclose(file);
 
                 return 0;
-            } else {
+            } else if (sortingMethod == 4){
                 int numberOfPartition = 1;
                 numberOfPartition += naturalSelection(file, "naturalSelectionPartition");
                 binaryTreeOfWinners(numberOfPartition, "naturalSelectionPartition");
@@ -119,6 +119,113 @@ int main() {
                 fclose(fileBinaryOfTreeSorted);
                 fclose(file);
 
+                return 0;
+            } else {
+
+                int numberOfPartition;
+                char partitionName[100];
+                char str1[100];
+                char str2[100] = ".dat";
+
+                printf("\nInsert number of partition for Hash Table (Hash Function): ");
+                scanf("%i", &numberOfPartition);
+
+                makePartitionHashTable(numberOfPartition);
+                hashTable(file, numberOfPartition, numFunc);
+
+                printf("Hash Table Successfully Created");
+
+                int flagAux = 0;
+
+                while (flagAux != 5) {
+
+                    printf("\n\nInput an option: \n 1 - Insert employee; \n 2 - Search Employee; \n 3 - Delete employee; \n 4 - Print employee IDs; \n 5 - Close program. \n ----> ");
+                    scanf("%i", &flagAux);
+
+                    if (flagAux == 1) {
+                        int employeeID;
+                        int partitionID;
+
+                        printf("\nInput employee ID: ");
+                        scanf("%i", &employeeID);
+
+                        TFunc funcHashIsert;
+
+                        if (employeeID < 0)
+                        {
+                            printf("\nErr: ID < 0");
+                            break;
+                        }
+
+                        funcHashIsert.id = employeeID;
+
+                        char auxName[15];
+                        for (int j = 0; j < 15; j++)
+                        {
+                            auxName[j] = 'a' + (char)(rand() % 26);
+                        }
+
+                        sprintf(funcHashIsert.name, "%s", auxName);
+                        sprintf(funcHashIsert.cpf, "111.111.111-11");
+                        sprintf(funcHashIsert.birthDate, "01/08/2021");
+                        funcHashIsert.pay = 1000 + rand() % 10000;
+
+                        partitionID = employeeID % numberOfPartition;
+
+                        insertHashTable(&funcHashIsert, employeeID, partitionID);
+
+
+                    } else if (flagAux == 2) {
+
+                        int employeeID;
+                        int partitionID;
+
+                        printf("\nInput employee id: ");
+                        scanf("%i", &employeeID);
+
+                        partitionID = employeeID % numberOfPartition;
+
+                        TFunc *funcSearchHashTable = searchHashTable(employeeID, partitionID);
+
+                        if (funcSearchHashTable != NULL) {
+                            printEmployee(funcSearchHashTable);
+                        } else {
+                            printf("\nEmployee id not found");
+                        }
+
+                    } else if (flagAux == 3) {
+
+                        int employeeID;
+                        int partitionID;
+
+                        printf("\nInput employee id: ");
+                        scanf("%i", &employeeID);
+
+                        partitionID = employeeID % numberOfPartition;
+
+                        deleteHashTable(employeeID, partitionID);
+
+                    } else if (flagAux == 4) {
+
+                        for (int i = 0; i < numberOfPartition; ++i) {
+
+                            itoa(i, str1, 10);
+                            strcat(strcpy(partitionName, "hashTablePartition"), str1);
+                            strcat(strcpy(partitionName, partitionName), str2);
+
+                            FILE *filePartition = fopen(partitionName,"rb+");
+
+                            printPartitionEmployeeID(filePartition, partitionName);
+
+                            fclose(filePartition);
+                        }
+
+                    } else {
+                        printf("Closing...");
+                    }
+                }
+
+                fclose(file);
                 return 0;
             }
 
@@ -169,7 +276,7 @@ int main() {
                 printEmployee(func);
             }
 
-            printf("\nInput Sorting method: \n 1 - InsertionSort;\n 2 - KeySort;\n 3 - Substitution Selection and MergeSort;\n 4 - Natural Selection and Binary Tree Of Winners;\n  --->  ");
+            printf("\nInput Sorting method: \n 1 - InsertionSort;\n 2 - KeySort;\n 3 - Substitution Selection and MergeSort;\n 4 - Natural Selection and Binary Tree Of Winners;\n 5 - Hash Table.\n  --->  ");
             scanf("%i", &sortingMethod);
 
             if (sortingMethod == 1) {
@@ -238,7 +345,7 @@ int main() {
                 fclose(file);
 
                 return 0;
-            } else {
+            } else if (sortingMethod == 4) {
                 int numberOfPartition = 1;
                 numberOfPartition += naturalSelection(file, "naturalSelectionPartition");
                 binaryTreeOfWinners(numberOfPartition, "naturalSelectionPartition");
@@ -251,6 +358,116 @@ int main() {
                 fclose(file);
 
                 return 0;
+            } else {
+
+                int numberOfPartition;
+                int contSizeFile = 0;
+                contSizeFile = sizeFile(file, contSizeFile);
+                char partitionName[100];
+                char str1[100];
+                char str2[100] = ".dat";
+
+                printf("\nInsert number of partition for Hash Table (Hash Function): ");
+                scanf("%i", &numberOfPartition);
+
+                makePartitionHashTable(numberOfPartition);
+                hashTable(file, numberOfPartition, contSizeFile);
+
+                printf("Hash Table Successfully Created");
+
+                int flagAux = 0;
+
+                while (flagAux != 5) {
+
+                    printf("\n\nInput an option: \n 1 - Insert employee; \n 2 - Search Employee; \n 3 - Delete employee; \n 4 - Print employee IDs; \n 5 - Close program. \n ----> ");
+                    scanf("%i", &flagAux);
+
+                    if (flagAux == 1) {
+                        int employeeID;
+                        int partitionID;
+
+                        printf("\nInput employee ID: ");
+                        scanf("%i", &employeeID);
+
+                        TFunc funcHashIsert;
+
+                        if (employeeID < 0)
+                        {
+                            printf("\nErr: ID < 0");
+                            break;
+                        }
+
+                        funcHashIsert.id = employeeID;
+
+                        char auxName[15];
+                        for (int j = 0; j < 15; j++)
+                        {
+                            auxName[j] = 'a' + (char)(rand() % 26);
+                        }
+
+                        sprintf(funcHashIsert.name, "%s", auxName);
+                        sprintf(funcHashIsert.cpf, "111.111.111-11");
+                        sprintf(funcHashIsert.birthDate, "01/08/2021");
+                        funcHashIsert.pay = 1000 + rand() % 10000;
+
+                        partitionID = employeeID % numberOfPartition;
+
+                        insertHashTable(&funcHashIsert, employeeID, partitionID);
+
+
+                    } else if (flagAux == 2) {
+
+                        int employeeID;
+                        int partitionID;
+
+                        printf("\nInput employee id: ");
+                        scanf("%i", &employeeID);
+
+                        partitionID = employeeID % numberOfPartition;
+
+                        TFunc *funcSearchHashTable = searchHashTable(employeeID, partitionID);
+
+                        if (funcSearchHashTable != NULL) {
+                            printEmployee(funcSearchHashTable);
+                        } else {
+                            printf("\nEmployee id not found");
+                        }
+
+                    } else if (flagAux == 3) {
+
+                        int employeeID;
+                        int partitionID;
+
+                        printf("\nInput employee id: ");
+                        scanf("%i", &employeeID);
+
+                        partitionID = employeeID % numberOfPartition;
+
+                        deleteHashTable(employeeID, partitionID);
+
+                    } else if (flagAux == 4) {
+
+                        for (int i = 0; i < numberOfPartition; ++i) {
+
+                            itoa(i, str1, 10);
+                            strcat(strcpy(partitionName, "hashTablePartition"), str1);
+                            strcat(strcpy(partitionName, partitionName), str2);
+
+                            FILE *filePartition = fopen(partitionName,"rb+");
+
+                            printPartitionEmployeeID(filePartition, partitionName);
+
+                            fclose(filePartition);
+                        }
+
+                    } else {
+                        printf("Closing...");
+                    }
+                }
+
+                fclose(file);
+                return 0;
+
             }
 
             printf("\nThe elapsed time in Sequential Fetch is %filePartition seconds.\n", timeSpentDefault);
