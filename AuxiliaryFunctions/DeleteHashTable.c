0cf31239a6e04e0e02db959cc3f450a3cb0e2ae7
Diogo Leite <diogoleite87@gmail.com>
2022-10-12 10:35:45 -0300

    fix: deleted register position

--- AuxiliaryFunctions/DeleteHashTable.c
+++ AuxiliaryFunctions/DeleteHashTable.c
@@ -9,13 +9,13 @@ void deleteHashTable(int employerID, int partitionID) {
     char partitionName[100];
     char str1[100];
     char str2[100] = ".dat";
-    int totalCompararisons;
+    int totalCompararisons = 0;
 
     itoa(partitionID, str1, 10);
     strcat(strcpy(partitionName, "hashTablePartition"), str1);
     strcat(strcpy(partitionName, partitionName), str2);
 
-    FILE *filePartition = fopen(partitionName,"ab+");
+    FILE *filePartition = fopen(partitionName,"r+b");
 
     TFunc *auxFunc = sequentialFetch(employerID, filePartition, &totalCompararisons);
 
@@ -25,7 +25,7 @@ void deleteHashTable(int employerID, int partitionID) {
 
         TFunc funcHashDelet;
 
-        funcHashDelet.id = 999999;
+        funcHashDelet.id = 999999999;
         sprintf(funcHashDelet.name, "%s", auxFunc->name);
         sprintf(funcHashDelet.cpf, "%s", auxFunc->cpf);
         sprintf(funcHashDelet.birthDate, "%s", auxFunc->birthDate);
